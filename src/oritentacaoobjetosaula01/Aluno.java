c0111cc80324ef5ebe2a8b75cd2f83d6a9fac106
dxt.glourenco <dxt.glourenco@ifood3rd.com>
2021-06-28 21:28:12 -0300

    basico de orientacao a objetos

--- src/oritentacaoobjetosaula01/Aluno.java
+++ src/oritentacaoobjetosaula01/Aluno.java
@@ -1,11 +1,50 @@
 package oritentacaoobjetosaula01;
 
 public class Aluno {
-    String nome;
-    String cpfOuRa;
-    int idade;
-    int[] notas;
+    private String nome;
+    private String cpfOuRa;
+    private int idade;
+    private int[] notas;
 
     public Aluno() {
+    }
+
+    public Aluno(String nome, String cpfOuRa, int idade, int[] notas) {
+        this.nome = nome;
+        this.cpfOuRa = cpfOuRa;
+        this.idade = idade;
+        this.notas = notas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpfOuRa() {
+        return cpfOuRa;
+    }
+
+    public void setCpfOuRa(String cpfOuRa) {
+        this.cpfOuRa = cpfOuRa;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int[] getNotas() {
+        return notas;
+    }
+
+    public void setNotas(int[] notas) {
+        this.notas = notas;
     }
 }
--- src/oritentacaoobjetosaula01/ExecutaAluno.java
+++ src/oritentacaoobjetosaula01/ExecutaAluno.java
@@ -3,13 +3,9 @@ package oritentacaoobjetosaula01;
 public class ExecutaAluno {
 
     public static void main(String[] args) {
-        Aluno alunoGuilherme = new Aluno();
-        alunoGuilherme.idade = 29;
-        alunoGuilherme.nome = "Guilherme";
-        alunoGuilherme.cpfOuRa = "1234545";
-        alunoGuilherme.notas = new int[]{1,2,3};
+        Aluno alunoGuilherme = new Aluno("Guilherme", "1234545", 29, new int[]{1,2,3});
 
-        int[] notas = alunoGuilherme.notas;
+        int[] notas = alunoGuilherme.getNotas();
 
         int total = 0;
 
@@ -17,9 +13,9 @@ public class ExecutaAluno {
             total += nota;
         }
 
-        System.out.println("A média de notas do " + alunoGuilherme.nome);
+        System.out.println("A média de notas do " + alunoGuilherme.getNome());
         System.out.println("é: " +  total / notas.length);
 
-        System.out.println("O aluno " + alunoGuilherme.nome + " tem " + alunoGuilherme.idade + " anos");
+        System.out.println("O aluno " + alunoGuilherme.getNome() + " tem " + alunoGuilherme.getIdade() + " anos");
     }
 }
